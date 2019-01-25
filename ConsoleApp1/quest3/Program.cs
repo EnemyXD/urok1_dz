0acc9fda0ddb38eca35adb8bc2060fd67bf8e41d
enemy <nkalmykov937@gmail.com>
2019-01-25 20:46:20 +0300

    доделал задание 3 со звездочкой и сделал задание 4

--- ConsoleApp1/quest3/Program.cs
+++ ConsoleApp1/quest3/Program.cs
@@ -27,11 +27,28 @@ namespace quest3
             var y2var = Console.ReadLine();
             double y2 = double.Parse(y2var);
 
+            //Расчет обычным способом
             double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
             Console.WriteLine($"{r:F2}");
 
+            //Расчет методом
+            
+            double z = Raschet(x1, x2, y1, y2);
+
+            Console.WriteLine($"{z:F2} с помощью метода");
+
             Console.ReadKey();
+        }
+
+
+
+        static double Raschet(double a1, double a2, double b1, double b2)
+        {
+     
+            double q = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            return q;
+
         }
     }
 }
