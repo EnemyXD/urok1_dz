86dbee9249293ad00600f0c36a1cad680cb7667b
enemy <nkalmykov937@gmail.com>
2019-01-25 18:17:43 +0300

    сделал Анкету

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+        
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Какая у вас фамилия?");
+            string second_name = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Какой у вас рост?");
+            var height = Console.ReadLine();
+
+            Console.WriteLine("Какой у вас вес?");
+            var weight = Console.ReadLine();
+
+            Console.WriteLine(name + " " + second_name + " " + age + " " + height + " " + weight);
+            Console.WriteLine("{0} {1} {2} {3} {4}", name, second_name, age, height, weight);
+            Console.WriteLine($"{name} {second_name} {age} {height} {weight}");
 
 
+            Console.ReadLine();
 
         }
     }
